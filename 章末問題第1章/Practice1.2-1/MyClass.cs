0613cb13f46f219e4ed6c365e58a3ab603f100f5
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-13 10:52:17 +0900

    章末問題第1章1.1-1～1.2-1までを実装しました

---
+++ 章末問題第1章/Practice1.2-1/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1._2_1 {
+    //クラス
+    class MyClass {
+        public string X { get; set;}
+        public int Y { get; set; }
+    }
+    //構造体
+    struct MyStruct{
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+}
---
+++ 章末問題第1章/Practice1.2-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1._2_1 {
+    class Program {
+        //MyClassとMyStructの2つの方を引数にとるメソッドPrintObjectsを定義してください。PrintObjectsメソッドでは、2つのオブジェクトの内容（プロパティの値)をコンソールに表示するようにしてください。なお、PrintObjectsメソッドはProgramクラスのメソッドとして定義してください。、
+        static void Main(string[] args) {
+            MyClass myClass = new MyClass { X = "1", Y = 2 };
+            MyStruct myStruct = new MyStruct { X = 1, Y = 2 };
+            PrintObjects(myClass,myStruct);
+            MyClass myClass2 = new MyClass { X = "10", Y = 20 };
+            MyStruct myStruct2 = new MyStruct { X = 100, Y = 200 };
+            PrintObjects(myClass2, myStruct2);
+            Console.ReadLine();
+        }
+
+
+
+        public static void PrintObjects(MyClass MC, MyStruct myStruct) {
+            Console.WriteLine("X =" + MC.X +"Y =" + MC.Y);
+            Console.WriteLine(myStruct.X + myStruct.Y);
+            
+        }
+       
+    }
+}
