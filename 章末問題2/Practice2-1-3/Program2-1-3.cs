ec2f441e11236a95d482a3751676ce4ba8a2efcc
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-18 16:04:46 +0900

    #455475 [C#テキスト]2章演習問題
    問1-3までを実装し、問1-4のファイルを作成しました、

---
+++ 章末問題2/Practice2-1-3/Program2-1-3.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice2_1_3 {
+    class Program {
+        //Practice2-1-3 作成したSongクラスのインスタンスを複数生成し、配列songsに格納してください。
+        static void Main(string[] args) {
+            Song song1 = new Song("白日", "KingGnu", 240);
+            Song song2 = new Song("アイドル", "YOASOBI", 210);
+            Song song3 = new Song("天体観測", "BUMP OF CHICKEN", 190);
+            //配列songsに格納
+            Song[] songs = { song1, song2, song3 };
+        }
+    }
+}
---
+++ 章末問題2/Practice2-1-4/Program2-1-4.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice2_1_4 {
+    class Program {
+        //配列に格納されたすべてのSongオブジェクトの内容をコンソールに出力してください。演奏時間の表示は「4:16」のような書式にしてください。ただし、演奏時間は必ず60分未満と仮定してかまいません。
+        static void Main(string[] args) {
+            Song song1 = new Song("白日", "KingGnu", 240);
+            Song song2 = new Song("アイドル", "YOASOBI", 250);
+            Song song3 = new Song("天体観測", "BUMP OF CHICKEN", 190);
+            //配列songsに格納
+            Song[] songs = { song1, song2, song3 };
+        }
+    }
+}
