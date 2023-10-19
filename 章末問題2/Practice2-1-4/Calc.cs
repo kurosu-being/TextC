adae6bf3e6a151d927ce3d2ba4537a13076ba0c9
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-19 11:08:33 +0900

    #455475 [C#テキスト]2章演習問題
    問1-4においてコーディング規約に則りメソッドにコメントを追加しました

--- 章末問題2/Practice2-1-4/Calc.cs
+++ 章末問題2/Practice2-1-4/Calc.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Practice2_1_4 {
     class Calc {
+        /// <summary>
+        /// 分を計算するメソッド
+        /// </summary>
+        /// <param name="vLength">曲の長さ</param>
+        /// <returns>曲の分/returns>
         public static int MinuteLength(int vLength){
             return vLength / 60;
         }
+        /// <summary>
+        /// 秒数を計算するメソッド
+        /// </summary>
+        /// <param name="vLength">曲の長さ</param>
+        /// <returns>曲の秒数</returns>
         public static int SecondLength(int vLength){
             return vLength % 60;
         }
--- 章末問題2/Practice2-1-4/Program2-1-4.cs
+++ 章末問題2/Practice2-1-4/Program2-1-4.cs
@@ -4,17 +4,16 @@ namespace Practice2_1_4 {
     class Program {
         //Practice2-1-4 配列に格納されたすべてのSongオブジェクトの内容をコンソールに出力してください。演奏時間の表示は「4:16」のような書式にしてください。ただし、演奏時間は必ず60分未満と仮定してかまいません。
         static void Main(string[] args) {
-            Song song1 = new Song("白日", "KingGnu", 2241);
-            Song song2 = new Song("アイドル", "YOASOBI", 275);
-            Song song3 = new Song("天体観測", "BUMP OF CHICKEN", 190);
+            Song wSong1 = new Song("白日", "KingGnu", 241);
+            Song wSong2 = new Song("アイドル", "YOASOBI", 275);
+            Song wSong3 = new Song("天体観測", "BUMP OF CHICKEN", 1190);
             //配列songsに格納
-            Song[] songs = { song1, song2, song3 };
+            Song[] songs = { wSong1, wSong2, wSong3 };
             // 配列に格納されたSongクラスのインスタンスをコンソールに出力
             foreach (Song song in songs) {
-                int wMinuteHyouji = Calc.MinuteLength(song.Length);
-                int wSecondHyouji = Calc.SecondLength(song.Length);
-                TimeSpan wTimeSpan = new TimeSpan(0, wMinuteHyouji, wSecondHyouji);
-                Console.WriteLine($"{song.Title}という曲のアーティストは {song.ArtistName}で曲の長さは{wTimeSpan.Minutes:D1}:{wTimeSpan.Seconds:D2}です");
+                int wShowMinute = Calc.MinuteLength(song.Length);
+                int wShowSecond = Calc.SecondLength(song.Length);
+                Console.WriteLine($"{song.Title}という曲のアーティストは {song.ArtistName}で曲の長さは{wShowMinute:D1}:{wShowSecond:D2}です");
             }
         }
     }
