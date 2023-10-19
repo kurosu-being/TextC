21efb33442f7d561e55d6cce244e1e78a8147240
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-19 11:43:33 +0900

    #455475 [C#テキスト]2章演習問題
    問2を実装しました。

---
+++ 章末問題2/Practice2-2-1/ConvertFeet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2_2_1 {
+    public static class ConvertFeet {
+        private const double C_Ratio = 0.0254;
+
+        /// <summary>
+        /// インチからメートルを求めるメソッド
+        /// </summary>
+        /// <param name="vMeter">メートル距離</param>
+        /// <returns>インチ距離</returns>
+        public static double FromMeter(double vMeter){
+            return vMeter * 0.0254;
+        }
+    }
+}
---
+++ 章末問題2/Practice2-2-1/Program2-2-1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2_2_1 {
+    class Program {
+        //[2.1:距離換算プログラム]のコードを三個王に、インチからメートルへの換算表を1インチ刻みでコンソールに出力するプログラムを書いてください。このときのインチの範囲は、1インチから10インチまでとしてください。1インチは0.0254メートルです。
+        static void Main(string[] args) {
+                PrintMeterToInchList(1,10);
+        }
+        /// <summary>
+        /// メートルからインチへの対応表を出力
+        /// </summary>
+        /// <param name="vStart">始めの数値</param>
+        /// <param name="vStop">終わりの数値</param>
+        static void PrintMeterToInchList(int vStart, int vStop){
+            for (int wMeter = vStart; wMeter <= vStop; wMeter++){
+                double wInch = ConvertFeet.FromMeter(wMeter);
+                Console.WriteLine($"{wMeter}m = {wInch:0.0000}in");
+            }
+        }
+    }
+}
