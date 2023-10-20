be37a8ea3a28653f01378eaa6245f7b590981668
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-20 10:17:34 +0900

    #455475 [C#テキスト]2章演習問題
    問1において不要なusingを削除しました。

--- 章末問題2/Practice2-1-2/Program2-1-2.cs
+++ 章末問題2/Practice2-1-2/Program2-1-2.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice2_1_2 {
+﻿namespace Practice2_1_2 {
     class Program {
         //Practice2-1-2 3つの引数を持つコンストラクタも設定してください
         static void Main(string[] args) {
