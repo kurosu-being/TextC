4c877df874c470867bba1d4afaf2f9828a458f1f
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-20 14:48:55 +0900

    #455475 [C#テキスト]2章演習問題
    Practice3-1-3を実装しました。

--- 章末問題3/Practice3-1-2/Program3-1-2.cs
+++ 章末問題3/Practice3-1-2/Program3-1-2.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice3_1_2 {
     class Program {
         //List<T>のメソッドを使い、書く要素を2.0で割った値をコンソールに出力してください。
         static void Main(string[] args) {
             var wNumbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
-            wNumbers.ForEach(s => Console.WriteLine(s /2.0));
+            wNumbers.ForEach(s => Console.WriteLine(s / 2.0));
         }
     }
 }
---
+++ 章末問題3/Practice3-1-3/Program3-1-3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice3_1_3 {
+    class Program {
+        //LINQのWhereメソッドを使い、値が50以上の要素を列挙し、その結果をコンソールに出力してください。
+        static void Main(string[] args) {
+            var wNumbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+            var wWhere = wNumbers.Where(s => s >= 50);
+            foreach (int wN in wWhere) {
+                Console.WriteLine(wN);
+            }
+        }
+    }
+}
