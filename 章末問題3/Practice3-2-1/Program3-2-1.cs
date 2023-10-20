1f0ec765397f6a7e43e51eadc14a95673e91c7e3
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-20 16:41:21 +0900

    #456425 [C#テキスト]3章演習問題
    Practice3-2-1を実装しました。

--- 章末問題3/Practice3-2-1/Program3-2-1.cs
+++ 章末問題3/Practice3-2-1/Program3-2-1.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice3_2_1 {
     class Program {
+        //以下のリストが定義してあります
+        //var names = new List<string>{
+        //"Tokyo","New Delhi", "Bangkok", "London", "Paris", "Berlin","Canberra"."Hong Kong",
+        //};
+        //このリストに対して、ラムダ式を使い次のコードを書いてください。
+        //Practice3-2-1 コンソールから入力した歳名が何番目に格納されているかList<T>のFindINdexメソッドを使って調べ、その結果おｗコンソールに出力してください。見つからなかったら、-1を出力してください。なお、コンソールの入力にはConsole.ReadLineメソッドを利用してください。
+        //コーディング規約に則りnames →　wNamesに変更
         static void Main(string[] args) {
+            var wNames = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong" };
+            string wInputuser = Console.ReadLine();
+            var wIndex = wNames.FindIndex(s => s == wInputuser);
+            if (wIndex != -1) {
+                Console.WriteLine($"インデックス番号は {wIndex}");
+            } else {
+                Console.WriteLine("-1");
+            }
         }
     }
 }
