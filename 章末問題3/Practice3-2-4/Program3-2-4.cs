5ba8b0dab1264c32acd740ff1588e8947d7cd582
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-23 13:38:16 +0900

    #456425 [C#テキスト]3章演習問題
    Practice3-2-4を実装しました。

---
+++ 章末問題3/Practice3-2-4/Program3-2-4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice3_2_4 {
+    class Program {
+        //LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数抽出し、その文字数をコンソールに出力してください。都市名を表示する必要はありません。
+        static void Main(string[] args) {
+            var wNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong" };
+            var wWhere = wNames.Where(s => s[0] == 'B');
+            var wNum = wWhere.Select(s => s.Length);
+            foreach (var wN in wNum) {
+                Console.WriteLine(wN);
+            }
+        }
+    }
+}
+
