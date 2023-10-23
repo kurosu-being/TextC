2712db3a126865d63518259196a0fa4085951f4d
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-23 16:10:30 +0900

    #456921 [C#テキスト]4章演習問題
    Practice4-1-1を実装しました。

---
+++ 章末問題4/Practice4-1-1/YearMonth.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice4_1_1 {
+    /// <summary>
+    /// 年(Year)と月(Month)の2つのプロパティを持つクラス
+    /// </summary>
+    class YearMonth {
+        /// <summary>
+        /// 年
+        /// </summary>
+        public int Year { get; private set; }
+        /// <summary>
+        /// 月
+        /// </summary>
+        public int Month { get; private set; }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="vYear">年</param>
+        /// <param name="vMonth">月</param>
+        public YearMonth(int vYear, int vMonth) {
+            this.Year = vYear;
+            this.Month = vMonth;
+        }
+    }
+}
