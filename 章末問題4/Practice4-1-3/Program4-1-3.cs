9f067084cd72351b3985e334ad0ad01325bae07c
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-23 17:42:05 +0900

    #456921 [C#テキスト]4章演習問題
    Practice4-1-3を実装しました。

---
+++ 章末問題4/Practice4-1-3/Program4-1-3.cs
@@ -0,0 +1,7 @@
+﻿namespace Practice4_1_3 {
+    class Program {
+        //YearMonthクラスに、1ヶ月後を求めるAddOneメソッドを追加してください。この時、自分自身のプロパティは変更せずに、新たなYearMonthオブジェクトを生成しその値を返してください。12月の時の処理に注意してください。
+        static void Main(string[] args) {
+        }
+    }
+}
---
+++ 章末問題4/Practice4-1-3/YearMonth.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice4_1_3 {
+    /// <summary>
+    /// 年(Year)と月(Month)の2つのプロパティを持つクラス
+    /// </summary>
+    class YearMonth {
+        /// <summary>
+        /// 年
+        /// </summary>
+        public int Year { get; private set; }
+        /// <summary>
+        /// 月
+        /// </summary>
+        public int Month { get; private set; }
+        /// <summary>
+        /// 21世紀
+        /// </summary>
+        public int Is21Century { get; set; }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="vYear">年</param>
+        /// <param name="vMonth">月</param>
+        public YearMonth(int vYear, int vMonth) {
+            this.Year = vYear;
+            this.Month = vMonth;
+        }
+        /// <summary>
+        /// 1か月後を求めるメソッド
+        /// </summary>
+        /// <returns>1か月後の年と日</returns>
+        public YearMonth AddOneMonth() {
+            int wNewYear = this.Year;
+            int wNewMonth = this.Month++;
+            if (wNewMonth > 12) {
+                wNewMonth = 1;
+                wNewYear++;
+            }
+            return new YearMonth(wNewYear, wNewMonth);
+        }
+    }
+}
+
