cbf79b1c402aa109dddf4601d3d3142a6fe2f054
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-24 14:16:07 +0900

    #456921 [C#テキスト]4章演習問題
    Practice4-1-4を実装しました。

---
+++ 章末問題4/Practice4-1-4/Program4-1-4.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Practice4_1_4 {
+    class Program {
+        //Practice4-1-4 ToStringメソッドをオーバーライドしてください。結果は"2017年8月"といった形式にしてください。
+        static void Main(string[] args) {
+            YearMonth wYearMonth = new YearMonth (2000, 3);
+            Console.WriteLine(wYearMonth);
+        }
+    }
+}
