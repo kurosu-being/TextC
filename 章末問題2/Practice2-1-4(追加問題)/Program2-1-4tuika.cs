73a5993a837587df7cfd82421cb1a04ff81ec74d
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-24 13:07:29 +0900

    #455475 [C#テキスト]2章演習問題
    追加問題の問題文を追加しました。

--- 章末問題2/Practice2-1-4(追加問題)/Program2-1-4tuika.cs
+++ 章末問題2/Practice2-1-4(追加問題)/Program2-1-4tuika.cs
@@ -2,20 +2,22 @@
 
 namespace Practice2_1_4_追加問題_ {
     class Program {
-        //Practice2-1-4 配列に格納されたすべてのSongオブジェクトの内容をコンソールに出力してください。演奏時間の表示は「4:16」のような書式にしてください。ただし、演奏時間は必ず60分未満と仮定してかまいません。
+        //追加問題1 さらに新しくSongクラスのインスタンスを生成して、wSongsに追加する処理を書いてみてください。
+        //追加問題2 自作メソッドを使わずに、TimeSpan構造体を使って同等の処理を書いてみてください。※前のコードは残したままでよいですと仮定してかまいません。
         static void Main(string[] args) {
             Song wSong1 = new Song("白日", "KingGnu", 241);
             Song wSong2 = new Song("アイドル", "YOASOBI", 275);
             Song wSong3 = new Song("天体観測", "BUMP OF CHICKEN", 1190);
             //配列songsに格納
             Song[] wSongs = { wSong1, wSong2, wSong3 };
-            // 追加問題１新しい曲を作成
+            // 追加問題１Songクラスのインスタンスを生成
             Song wSong4 = new Song("君が代", "林広守", 240);
             // 配列のサイズを拡張して新しい曲を追加
             Array.Resize(ref wSongs, wSongs.Length + 1);
             wSongs[wSongs.Length - 1] = wSong4;
             // 配列に格納されたSongクラスのインスタンスをコンソールに出力
             foreach (Song wSong in wSongs) {
+                //追加問題2 TimeSpan構造体を利用
                 TimeSpan wTimeSpan = TimeSpan.FromSeconds(wSong.Length);
                 Console.WriteLine($"{wSong.Title}という曲のアーティストは {wSong.ArtistName}で曲の長さは{wTimeSpan.Minutes:D1}:{wTimeSpan.Seconds:D2}です");
             }
