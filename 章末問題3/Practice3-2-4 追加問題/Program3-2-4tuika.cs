160459bb52311e6c5660af18f3aac20849497063
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-26 16:12:38 +0900

    #456425 [C#テキスト]3章演習問題
    Practice3-2-4において追加問題を修正しました。

--- 章末問題3/Practice3-2-4 追加問題/Program3-2-4tuika.cs
+++ 章末問題3/Practice3-2-4 追加問題/Program3-2-4tuika.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Practice3_2_4_追加問題 {
     class Program {
+        //LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数抽出し、その文字数をコンソールに出力してください。都市名を表示する必要はありません。
         static void Main(string[] args) {
+            var wCityNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", "" };
+            var wMatchNames = wCityNames.Where(s => s.LastOrDefault() == 'n')?.OrderBy(x => x[0]);
+            foreach (string wMessage2 in wMatchNames) {
+                Console.WriteLine(wMessage2);
+            }
         }
     }
 }
--- 章末問題3/Practice3-2-4/Program3-2-4.cs
+++ 章末問題3/Practice3-2-4/Program3-2-4.cs
@@ -6,10 +6,15 @@ namespace Practice3_2_4 {
     class Program {
         //LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数抽出し、その文字数をコンソールに出力してください。都市名を表示する必要はありません。
         static void Main(string[] args) {
-            var wCityNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong" ,""};
-            var wMatchNames = wCityNames.Where(s => s.FirstOrDefault() == 'B')?.Select(s => s.Length) ;
-            foreach (int wMessage in wMatchNames) {
+            var wCityNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", "Edmonton" };
+            //wCityNames.Where(s => s.FirstOrDefault() == 'B')?.Select(s => s.Length)の記述が長いと判断したため、変数を定義しました。
+            var wFirstS = wCityNames.Where(s => s.FirstOrDefault() == 'B')?.Select(s => s.Length);
+            foreach (int wMessage in wFirstS) {
                 Console.WriteLine(wMessage);
+            }
+            var wLastN = wCityNames.Where(s => s.LastOrDefault() == 'n')?.OrderBy(x => x[0]);
+            foreach (string wMessage2 in wLastN) {
+                Console.WriteLine(wMessage2);
             }
         }
     }
