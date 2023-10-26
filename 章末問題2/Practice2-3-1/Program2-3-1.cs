bcec50638fefd3eec7873b27cb8d5f6d6f84db2c
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-26 11:01:07 +0900

    #455475 [C#テキスト]2章演習問題
    Practice2-3-1において、プログラムの引数として受け取れるように、最初のコンソール画面でユーザーが自身の保存したファイルのパスを入力する形に変更しました。

--- 章末問題2/Practice2-3-1/Program2-3-1.cs
+++ 章末問題2/Practice2-3-1/Program2-3-1.cs
@@ -4,7 +4,9 @@ namespace Practice2_3_1 {
     class Program {
         //「2.2:売上集計プログラム」で作成したプログラムを変更し、商品カテゴリ別の売上高を求めるプログラムを作成してください
         static void Main(string[] args) {
-            var wSales = new SalesCounter(@"../../sales.csv");
+            Console.WriteLine("sales.csvのパスを入力してください(sales.csvファイルはチケットに貼っています)");
+            string wInputText = Console.ReadLine();
+            var wSales = new SalesCounter(wInputText);
             foreach (var wObj in wSales.GetPerItemSales()) {
                 Console.WriteLine($"{wObj.Key}は合計{wObj.Value}円です");
             }
