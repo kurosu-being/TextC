15b6d3502e43cb06189097c58655adf4c97dfef1
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-27 13:45:05 +0900

    #456425 [C#テキスト]3章演習問題
    ファイル全体で変数の命名を変更しました。
    Practice3-2-4において、OrderByのラムダ式を変更しました、

--- 章末問題3/Practice3-2-4 追加問題/Program3-2-4tuika.cs
+++ 章末問題3/Practice3-2-4 追加問題/Program3-2-4tuika.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Practice3_2_4_追加問題 {
     class Program {
-        //LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数抽出し、その文字数をコンソールに出力してください。都市名を表示する必要はありません。
+        //追加問題　'n' で終わる都市名を抽出し、アルファベット順にならべたものを出力してください。
         static void Main(string[] args) {
             var wCityNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", "" };
             var wMatchNames = wCityNames.Where(s => s.LastOrDefault() == 'n').OrderBy(x => x[0]);
-            foreach (string wMessage2 in wMatchNames) {
-                Console.WriteLine(wMessage2);
+            foreach (string wMatchName in wMatchNames) {
+                Console.WriteLine(wMatchName);
             }
         }
     }
--- 章末問題3/Practice3-2-4/Program3-2-4.cs
+++ 章末問題3/Practice3-2-4/Program3-2-4.cs
@@ -6,15 +6,14 @@ namespace Practice3_2_4 {
     class Program {
         //LINQのWhereメソッドとSelectメソッドを使い、'B'で始まる都市名の文字数抽出し、その文字数をコンソールに出力してください。都市名を表示する必要はありません。
         static void Main(string[] args) {
-            var wCityNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", "Edmonton" };
-            //wCityNames.Where(s => s.FirstOrDefault() == 'B')?.Select(s => s.Length)の記述が長いと判断したため、変数を定義しました。
-            var wFirstS = wCityNames.Where(s => s.FirstOrDefault() == 'B').Select(s => s.Length);
-            foreach (int wMessage in wFirstS) {
-                Console.WriteLine(wMessage);
+            var wCityNames = new List<string> { "Tokyo", "ONew Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", "Edmonton", "LYon", "LYan", };
+            foreach (int wCityLengthS in wCityNames.Where(s => s.FirstOrDefault() == 'B').Select(s => s.Length)) {
+                Console.WriteLine(wCityLengthS);
             }
-            var wLastN = wCityNames.Where(s => s.LastOrDefault() == 'n').OrderBy(x => x[0]);
-            foreach (string wMessage2 in wLastN) {
-                Console.WriteLine(wMessage2);
+
+            //追加問題　同様に、'n' で終わる都市名を抽出し、アルファベット順にならべたものを出力してください。
+            foreach (string wCityLengthN in wCityNames.Where(s => s.LastOrDefault() == 'n').OrderBy(x => x)) {
+                Console.WriteLine(wCityLengthN);
             }
         }
     }
