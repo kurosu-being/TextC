406a01bb44982dc90c16e9788a8ef7796eeeebfe
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-10-30 16:20:48 +0900

    #458174 [C#テキスト]5章演習問題
    Practice5-2-1を実装しました。

--- 章末問題5/Practice5-1-1/Program5-1-1.cs
+++ 章末問題5/Practice5-1-1/Program5-1-1.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice5_1_1 {
     /*Practice5-1-1 コンソールから入力した2つの文字列が等しいか調べるコードを書いてください。
@@ -12,9 +8,9 @@ namespace Practice5_1_1 {
         static void Main(string[] args) {
             Console.WriteLine("[○○,■■」という形で2つの文字列を入力してください");
             string wInput = Console.ReadLine();
-            string wMessage = string.IsNullOrEmpty(wInput)
-            ? "入力が空です。文字列を入力してください。":(wInput.Split(',').Length == 2 && string.Compare(wInput.Split(',')[0], wInput.Split(',')[1], true) == 0)
-            ? "一致しています":"不一致です";
+            string wMessage = string.IsNullOrEmpty(wInput) 
+                ? "入力が空です。文字列を入力してください。" : (wInput.Split(',').Length == 2 && string.Compare(wInput.Split(',')[0], wInput.Split(',')[1], true) == 0)
+                ? "一致しています" : "不一致です。\n或いは入力された文字列の数が指定と違います";
             Console.WriteLine(wMessage);
         }
     }
--- 章末問題5/Practice5-2-1/Program5-2-1.cs
+++ 章末問題5/Practice5-2-1/Program5-2-1.cs
@@ -5,8 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Practice5_2_1 {
+    /*コンソールから入力した数字文字列をint型に変換したのち、カンマ付きの数字文字列に変換してください。
+    入力した文字列は、intTryParseメソッドで数値に変換してください。*/
     class Program {
         static void Main(string[] args) {
-        }
+            Console.Write("数字文字列を入力してください: ");
+            if (int.TryParse(Console.ReadLine(), out int wNumber)) {
+                string wFormatNumber = wNumber.ToString("#,0"); 
+                Console.WriteLine("カンマ付きの数字文字列: " + wFormatNumber);
+            } 
+            else {
+                Console.WriteLine("無効な数字文字列です。");
+            }
+
+            //Console.WriteLine("数字を入力してください");
+            //string wInput = Console.ReadLine();
+            //var wOutput = wInput;
+            //int wInt;
+            //if ( int.TryParse(wInput, out wInt)){
+            //    Console.WriteLine($"{wOutput.ToString("#")");
+            //}
+            
     }
+}
 }
