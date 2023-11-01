4bc8da1a5f834430a86dc32e40dc67749620f161
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-01 16:09:17 +0900

    #458174 [C#テキスト]5章演習問題
    コメントの区切りを追加して修正しました。

--- 章末問題5/Practice5-2-1/Program5-2-1.cs
+++ 章末問題5/Practice5-2-1/Program5-2-1.cs
@@ -2,7 +2,7 @@
 
 namespace Practice5_2_1 {
     /*Practice5-2-1 コンソールから入力した数字文字列をint型に変換したのち、カンマ付きの数字文字列に変換してください。
-    入力した文字列は、intTryParseメソッドで数値に変換してください。*/
+    入力した文字列は、int.TryParseメソッドで数値に変換してください。*/
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("整数を入力してください。3桁ごとにカンマ付きの数字文字列に変換します。: ");
