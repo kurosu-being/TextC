ce5b22383e802484c42c2827cb447381fb6c2556
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-02 16:57:22 +0900

    #458736 [C#テキスト]6章演習問題
    Practice6-2-7を実装しました。

---
+++ 章末問題6/Practice6-2-7/Program6-2-7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice6_2_7 {
+    //Practice6-2-7 booksの中で、タイトルに"C#"が含まれていてかつ500ページ以下の本を見つけ、本のタイトルを表示するコードを書いてください。複数見つかった場合は、その全てを表示してください。
+    class Program {
+        static void Main(string[] args) {
+            var wBooks = new List<Book>
+             {
+                new Book ("C#プログラミングの常識", 3800, 378 ),
+                new Book ("ラムダ式とLINQの極意",2500, 312 ),
+                new Book ( "ワンダフル・C#ライフ", 2900, 385 ),
+                new Book ( "一人で学ぶ並列処理プログラミング", 4800, 464 ),
+                new Book ( "フレーズで覚えるC#入門", 5300, 604 ),
+                new Book ( "私でも分かったASP.NET MVC", 3200, 453 ),
+                new Book ( "C#プログラミングの常識", 2540, 348 ),
+            };
+            var wTopThreePrice = wBooks.Where(x => x.Title.Contains("C#") && x.Pages <= 500);
+            Console.WriteLine("タイトルに「C#」が含まれていてかつ500ページ以下の本");
+            foreach (var wBook in wTopThreePrice) {
+                Console.WriteLine($"書籍: {wBook.Title}");
+            }
+
+        }
+    }
+}
