78e928f3dc69787e051d952cf60c08c7c4a76744
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-07 13:40:39 +0900

    #459134 [C#テキスト]7章演習問題
    Practice7-1-2を実装しました。
    Practice7-2-1のファイルを作成しました。

---
+++ 章末問題7/Practice7-1-2/Program7-1-2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice7_1_2 {
+    //Practice7-1-2 Practice7-1-1のプログラムを、SortedDictionary<TKey,TValue>を使って書き換えてください
+    class Program {
+        static void Main(string[] args) {
+            string wInputString = "Cozy lummox gives smart squid who asks for job pen";
+
+            wInputString = wInputString.ToUpper();
+            var wCharCount = new SortedDictionary<char, int>();
+
+            foreach (char wCharacter in wInputString) {
+                if (char.IsLetter(wCharacter)) {
+                    wCharCount[wCharacter] = wCharCount.ContainsKey(wCharacter) ? wCharCount[wCharacter] + 1 : wCharCount[wCharacter] = 1;
+                }
+            }
+
+            foreach (var wKeyValuePair in wCharCount) {
+                Console.WriteLine($"'{wKeyValuePair.Key}': {wKeyValuePair.Value}");
+            }
+        }
+    }
+}
