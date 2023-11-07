b99e4d0e1f1ad078e9130dcef88f76937b345f89
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-07 15:36:19 +0900

    #459134 [C#テキスト]7章演習問題
    Practice7-2-1の途中まで追加しました。

---
+++ 章末問題7/Practice7-2-1/Abbreviations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice7_2_1 {
+    class Abbreviations {
+        private Dictionary<string, string> _FDict = new Dictionary<string, string>();
+
+        public Abbreviations(){
+            var wLines = FileStyleUriParser.ReadAllLines("Abbreviations.txt");
+        }
+    }
+}
---
+++ 章末問題7/Practice7-2-1/Program7-2-1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice7_2_1 {
+    /*「7.3ディクショナリを使ったサンプルプログラム」で作成したプログラムに、以下の機能を追加してください。*/
+    //Practice7-2-1 ディクショナリに登録されている用語の数を示すCountプロパティをAbbreviationsクラスに追加してください。
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
