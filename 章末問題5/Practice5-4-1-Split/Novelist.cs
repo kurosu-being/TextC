2b927674294e457880426e99b8b91e29aa629690
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-07 10:38:24 +0900

    #458174 [C#テキスト]5章演習問題
    Novelistクラスのプロパティ、コンストラクタにコメントを追加しました。

--- 章末問題5/Practice5-4-1-Split/Novelist.cs
+++ 章末問題5/Practice5-4-1-Split/Novelist.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Practice5_4_1_Split {
     class Novelist {
+        /// <summary>
+        /// 作家
+        /// </summary>
         public string Writer { get; private set; }
+        /// <summary>
+        /// 代表作
+        /// </summary>
         public string BestWork { get; private set; }
+        /// <summary>
+        /// 誕生年
+        /// </summary>
         public int Born { get; private set; }
-
+        /// <summary>
+        /// Novelistクラスのコンストラクタ
+        /// </summary>
+        /// <param name="vSentence">受け取る文字列</param>
         public Novelist(string vSentence) {
             var parts = vSentence.Split(';')
-                .Select(part => part.Split('='))
-                .ToDictionary(pair => pair[0], pair => pair[1]);
-
+                .Select(x => x.Split('='))
+                .ToDictionary(p => p[0], p => p[1]);
             if (parts.ContainsKey("Novelist"))
                 this.Writer = parts["Novelist"];
             if (parts.ContainsKey("BestWork"))
