5a62eb208b9fe54dae6e4723d65be9a3245c1c6b
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-07 10:43:43 +0900

    #458174 [C#テキスト]5章演習問題
    Practice5-3-1において、不要な空行を削除しました。

--- 章末問題5/Practice5-3-1/Program5-3-1.cs
+++ 章末問題5/Practice5-3-1/Program5-3-1.cs
@@ -4,7 +4,6 @@ namespace Practice5_3_1 {
     /* "Jackdaws love my big sphinx quartz."という文字列があります。この文字列に対して、
        以下の問題を解いてください
        Practice5-3-1 空白が何文字あるかカウントしてください　*/
-
     class Program {
         static void Main(string[] args) {
             //確認の為、空白に半角スペース、全角スペース、Tabスペースを使用しています
