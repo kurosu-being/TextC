f9628cc4853f007db9c3243858c9fc94edacda42
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-09 14:11:37 +0900

    #459603 [C#テキスト]8章演習問題
    Practice8-1-1を実装しました。

---
+++ 章末問題8/Practice8-1-1/Program8-1-1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Practice8_1_1 {
+    //Practice8-1-1 現在の日時を以下のような3種類の書式でコンソールに出力してください。
+    /* ↓表示書式
+    2019/1/15 19:48
+    2019年01月15日 19時48分32秒
+    平成31年 1月15日(火曜日)
+    */
+    class Program {
+        static void Main(string[] args) {
+            var wDate = new DateTime(2019, 1, 15, 19, 48, 32);
+            var wCulture = new CultureInfo("ja-JP");
+            wCulture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var wJapanDay = wDate.ToString("ggyy年M月d日", wCulture);
+            var wDayOfWeek = wCulture.DateTimeFormat.GetDayName(wDate.DayOfWeek);
+
+            Console.WriteLine(wDate.ToString("f"));
+            Console.WriteLine(wDate.ToString("yyyy年MM月dd日 HH時mm分ss秒"));
+            Console.WriteLine($"{wJapanDay}({wDayOfWeek})");
+        }
+    }
+}
