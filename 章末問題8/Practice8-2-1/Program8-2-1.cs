613edf92bc6c04b25870270380ac3e15b0aac4d9
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-09 16:14:34 +0900

    #459603 [C#テキスト]8章演習問題
    Practice8-1-2を仮に実装しました。
    次の指定曜日を求めるメソッドをコメントにしました。

---
+++ 章末問題8/Practice8-2-1/Program8-2-1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice8_2_1 {
+    class Program {
+        static void Main(string[] args) {
+            var today = DateTime.Today;
+            DateTime nextWeekFriday = NextWeek(today, DayOfWeek.Friday);
+            Console.WriteLine(nextWeekFriday);
+        }
+        public static DateTime NextWeek(DateTime vDate, DayOfWeek vDayOfWeek) {
+            int wDaysSpan = ((int)vDayOfWeek - (int)vDate.DayOfWeek + 7) % 7;
+            return vDate.AddDays(wDaysSpan + 7);
+        }
+    }
+}
+//            var wToday = DateTime.Today;
+//            DateTime NextWeekWednesDay = NextWeek(wToday, DayOfWeek.Friday);
+//            Console.WriteLine(NextWeekWednesDay);
+//        }
+//        public static DateTime NextWeek(DateTime vDate, DayOfWeek vDayOfWeek){
+//            if ((int)vDayOfWeek - (int)(vDate.DayOfWeek) >= 0) {
+//            var wDays = (int)vDayOfWeek - (int)(vDate.DayOfWeek);
+//            if (wDays <= 0){
+//               wDays += 7;
+//            } else {
+//                wDays += 14;
+//                }
+//            return vDate.AddDays(wDays);
+//            }
+
+//        }
+//    }
+//}
