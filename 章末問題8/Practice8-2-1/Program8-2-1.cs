91f8fd1268ca5ce22e7517e273991d3ebc1d64e7
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-10 14:55:54 +0900

    #459603 [C#テキスト]8章演習問題
    Practice8-1-2を実装しました。
    Practice8-1-3のファイルを追加しました。

--- 章末問題8/Practice8-2-1/Program8-2-1.cs
+++ 章末問題8/Practice8-2-1/Program8-2-1.cs
@@ -1,33 +1,22 @@
 ﻿using System;
 
 namespace Practice8_2_1 {
+    //Practice8-2-1　p215「8.5.1:次の指定曜日を求める」のメソッドを参考に、次の週の指定曜日を求めるメソッドを定義してください。
     class Program {
         static void Main(string[] args) {
-            var today = DateTime.Today;
-            DateTime nextWeekFriday = NextWeek(today, DayOfWeek.Friday);
-            Console.WriteLine(nextWeekFriday);
+            var wToday = DateTime.Today;
+            DateTime wNextWeekFriday = NextWeek(wToday, DayOfWeek.Sunday);
+            Console.WriteLine(wNextWeekFriday);
         }
+        /// <summary>
+        /// 次の週の指定曜日を求めるメソッド
+        /// </summary>
+        /// <param name="vDate">日付</param>
+        /// <param name="vDayOfWeek">指定曜日</param>
+        /// <returns></returns>
         public static DateTime NextWeek(DateTime vDate, DayOfWeek vDayOfWeek) {
             int wDaysSpan = ((int)vDayOfWeek - (int)vDate.DayOfWeek + 7) % 7;
             return vDate.AddDays(wDaysSpan + 7);
         }
     }
 }
-//            var wToday = DateTime.Today;
-//            DateTime NextWeekWednesDay = NextWeek(wToday, DayOfWeek.Friday);
-//            Console.WriteLine(NextWeekWednesDay);
-//        }
-//        public static DateTime NextWeek(DateTime vDate, DayOfWeek vDayOfWeek){
-//            if ((int)vDayOfWeek - (int)(vDate.DayOfWeek) >= 0) {
-//            var wDays = (int)vDayOfWeek - (int)(vDate.DayOfWeek);
-//            if (wDays <= 0){
-//               wDays += 7;
-//            } else {
-//                wDays += 14;
-//                }
-//            return vDate.AddDays(wDays);
-//            }
-
-//        }
-//    }
-//}
