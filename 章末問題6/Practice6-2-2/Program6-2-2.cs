9481530260a2d3ca9f5147e6272797c1f35d7ef5
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-14 11:44:01 +0900

    #458736 [C#テキスト]6章演習問題
    Practice6-2-2において追加問題を仮実装しました。

--- 章末問題6/Practice6-2-2/Program6-2-2.cs
+++ 章末問題6/Practice6-2-2/Program6-2-2.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Practice6_2_2 {
     //Practice6-2-2 booksの中で、タイトルに"C#"が含まれている書籍が何冊あるかカウントするコードを書いてください。
@@ -9,20 +11,25 @@ namespace Practice6_2_2 {
         static void Main(string[] args) {
             var wBooks = new List<Book>
           {
-                new Book("ｃ#プログラミングの常識", 3800, 378),
+                //確認の為C#の表記を大文字小文字半角全角を試しています
+                new Book("C#プログラミングの常識", 3800, 378),
                 new Book("ラムダ式とLINQの極意", 2500, 312),
-                new Book("ワンダフル・C#ライフ", 2900, 385),
+                new Book("ワンダフル・c#ライフ", 2900, 385),
                 new Book("一人で学ぶ並列処理プログラミング", 4800, 464),
-                new Book("フレーズで覚えるC#入門", 5300, 604),
+                new Book("フレーズで覚えるｃ#入門", 5300, 604),
                 new Book("私でも分かったASP.NET MVC", 3200, 453),
-                new Book("楽しいｃ#プログラミング教室", 2540, 348),
+                new Book("楽しいＣ#プログラミング教室", 2540, 348),
             };
 
             if (wBooks == null) {
                 Console.WriteLine("コレクションの中身がnullです。");
                 return;
             }
-            Console.WriteLine($"タイトルに\"C#\"が含まれる書籍の数: {wBooks.Count(x => x.Title.ToUpper().Contains("C#"))}冊");
+
+            Console.WriteLine($"タイトルに\"C#\"が含まれる書籍の数: {wBooks.Count(x => x.Title.ToUpper().Normalize(NormalizationForm.FormKC).Contains("C#"))}冊");
         }
     }
 }
+
+
+
