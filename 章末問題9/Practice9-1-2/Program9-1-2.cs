96abe701934c32fe03a02ee3126faccca7a3244c
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-16 11:46:48 +0900

    #459842 [C#テキスト]9章演習問題
    Practice9-1-2を仮実装しました。

---
+++ 章末問題9/Practice9-1-2/Program9-1-2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace Practice9_1_2 {
+    class Program {
+        static void Main() {
+            Console.WriteLine("C#のソースファイルのパスを入力してください:");
+            string wFilePath = Console.ReadLine();
+
+            if (!File.Exists(wFilePath)) {
+                Console.WriteLine("ファイルが見つかりませんでした。");
+                return;
+            }
+
+            int wClassCount = 0;
+
+            using (var wReader = new StreamReader(wFilePath)) {
+                while (!wReader.EndOfStream) {
+                    int wCount = File.ReadAllLines(wFilePath, Encoding.UTF8).Count(x => x.Contains("class"));
+                    wClassCount = wCount;
+                }
+            }
+
+            Console.WriteLine($"\"class\"キーワードが含まれる行数: {wClassCount}");
+        }
+    }
+}
