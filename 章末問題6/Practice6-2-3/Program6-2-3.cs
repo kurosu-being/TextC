94664a36898c74c36212447ec6e5ecf7d4bd1e56
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-17 11:51:30 +0900

    #458736 [C#テキスト]6章演習問題
    Practice6-2-3ににおいて、Titleのnullチェックを追加しました。

--- 章末問題6/Practice6-2-3/Program6-2-3.cs
+++ 章末問題6/Practice6-2-3/Program6-2-3.cs
@@ -8,13 +8,15 @@ namespace Practice6_2_3 {
     class Program {
         static void Main(string[] args) {
             var wBooks = new List<Book>{
-                new Book("C#プログラミングの常識", 3800, 378),
-                new Book("ラムダ式とLINQの極意", 2500, 312),
-                new Book("ワンダフル・C#ライフ", 2900, 385),
-                new Book("一人で学ぶ並列処理プログラミング", 4800, 464),
-                new Book("フレーズで覚えるC#入門", 5300, 604),
-                new Book("私でも分かったASP.NET MVC", 3200, 453),
-                new Book("楽しいC#プログラミング教室", 2540, 348),
+                new Book(null, 345,345),
+                //new Book("C#プログラミングの常識", 3800, 378),
+                //new Book("ラムダ式とLINQの極意", 2500, 312),
+                //new Book("ワンダフル・C#ライフ", 2900, 385),
+                //new Book("一人で学ぶ並列処理プログラミング", 4800, 464),
+                //new Book("フレーズで覚えるC#入門", 5300, 604),
+                //new Book("私でも分かったASP.NET MVC", 3200, 453),
+                //new Book("楽しいC#プログラミング教室", 2540, 348),
+                
             };
 
             if (wBooks == null) {
@@ -22,8 +24,8 @@ namespace Practice6_2_3 {
                 return;
             }
 
-            var wMatchingBook = wBooks.FirstOrDefault(x => x.Title.Contains("C#"));
-            string wMessage = wMatchingBook != null ? $"タイトルに「C#」が含まれる書籍の平均ページ数: {wBooks.Average(x => x.PageNumber)}P" : "一致する書籍が存在しません。";
+            var wMatchingBook = wBooks.Where(x => x.Title != null && x.Title.Contains("C#"));
+            string wMessage = wMatchingBook.Any() ? $"タイトルに「C#」が含まれる書籍の平均ページ数: {wMatchingBook.Average(x => x.PageNumber)}P" : "一致する書籍が存在しません。";
 
             Console.WriteLine(wMessage);
         }
