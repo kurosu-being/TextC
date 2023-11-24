c993d643c7df1b024c4f6e9c87dc0eb4c4326d38
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-24 16:48:50 +0900

    #461377 [C#テキスト]10章演習問題
    Practice10-2-1を実装しました。

--- 章末問題10/Practice10-1-1/Program10-1-1.cs
+++ 章末問題10/Practice10-1-1/Program10-1-1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Practice10_1_1 {
---
+++ 章末問題10/Practice10-2-1/Program10-2-1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Practice10_2_1 {
+    //Practice10-2-1 テキストファイルを読み込み三文字以上の文字だけから成る部分文字列をすべて抜き出すコードを書いてください。
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("テキストファイルのパスを書いてください");
+            var wFilePath = Console.ReadLine();
+
+            if (!File.Exists(wFilePath)) {
+                Console.WriteLine("ファイルが存在しません");
+                return;
+            }
+
+            string wText = File.ReadAllText(wFilePath);
+
+            List<string> wSubstrings = PickNumbers(wText);
+
+            if (wSubstrings.Count > 0) {
+                Console.WriteLine("3文字以上の部分文字列:");
+                foreach (string wSubstring in wSubstrings) {
+                    Console.WriteLine(wSubstring);
+                }
+            } else {
+                Console.WriteLine("3文字以上の部分文字列が見つかりませんでした。");
+            }
+        }
+        static List<string> PickNumbers(string vText) {
+
+            var wMatches = Regex.Matches(vText, @"\b(?![0-9])\w{3,}\b");
+            var wSubstrings = new List<string>();
+
+            foreach (Match wMatch in wMatches) {
+                wSubstrings.Add(wMatch.Value);
+            }
+
+            return wSubstrings;
+        }
+    }
+}
