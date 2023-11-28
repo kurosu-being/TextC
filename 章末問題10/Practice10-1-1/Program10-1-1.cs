5f84d2faec00e7ca0113d174f1b03c6eeb224ce5
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-28 15:24:11 +0900

    #461377 [C#テキスト]10章演習問題
    Practice10-1-1において、コンソールに出力するメッセージを修正しました。

--- 章末問題10/Practice10-1-1/Program10-1-1.cs
+++ 章末問題10/Practice10-1-1/Program10-1-1.cs
@@ -5,7 +5,7 @@ namespace Practice10_1_1 {
     //Practice10-1-1 指定された文字列が電話番号かどうか判定するメソッドを定義してください。電話番号は必ずハイフン(-)で区切られていなければなりません。また、先頭三文字は"090","080","070"のいずれかとします。
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("電話番号を含む文字列を半角で入力してください。電話番号は必ずハイフン(-)で区切");
+            Console.WriteLine("電話番号を含む文字列を半角で入力してください。電話番号は必ずハイフン(-)で区切ってください。");
             var wInput = Console.ReadLine();
             if (string.IsNullOrEmpty(wInput)) {
                 Console.WriteLine("入力が空です");
