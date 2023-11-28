6e83744ab9fbdf4c71a3a14ad1e225fbf1e9e37c
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-28 09:39:04 +0900

    #461377 [C#テキスト]10章演習問題
    Practice10-6-1において、正規表現を修正しました。

--- 章末問題10/Practice10-6-1/Program10-6-1.cs
+++ 章末問題10/Practice10-6-1/Program10-6-1.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Practice10_6_1 {
     //Practice10-6-1 ５文字の回文とマッチする正規表現を書いてください。数字や記号だけからなる回文を除外するにはどうしたら良いのかも考えてください。
     class Program {
         static void Main(string[] args) {
             var wText = "しんぶんし　るすにする しるし　12321 godog ^^:^^ ";
-            var wPattern = @"\b(?![0-9])(\w)(\w)\w\2\1\b";
+            var wPattern = @"\b(?![\W\d]+)(\w)(\w)\w\2\1\b";
             var wMatches = Regex.Matches(wText, wPattern);
-            foreach (Match wMatch in wMatches){
+            foreach (Match wMatch in wMatches) {
                 Console.WriteLine(wMatch);
             }
         }
