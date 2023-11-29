69a56b3dbdc3f677654d4a525656e6dd07e0a64a
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-29 15:28:54 +0900

    #462103 [C#テキスト]11章演習問題
    Practice11-1-1の.NetFrameWorkを2.6.1→2.7.1に変更しました。
    Practice11-1-3を実装しました。

--- 章末問題11/Practice11-1-3/Program11-1-3.cs
+++ 章末問題11/Practice11-1-3/Program11-1-3.cs
@@ -1,13 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Practice11_1_3 {
-    //Practice11-1-3 メンバー人数が最も多い協議名を表示してください。
+    //Practice11-1-3 メンバー人数が最も多い競技名を表示してください。
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("XMLファイルのパスを入力してください。");
+            var wInputFile = Console.ReadLine();
+            if (!File.Exists(wInputFile)) {
+                Console.WriteLine("ファイルが存在しません。パスが正しいか確認してください。");
+            }
+
+            var wXdocument = XDocument.Load(wInputFile);
+            var wMaxMembersSport = wXdocument.Root.Elements()
+                            .Where(x => (int)x.Element("teammembers") == wXdocument.Root.Elements().Max(y => (int)y.Element("teammembers")))
+                            .Select(x => x.Element("name")?.Attribute("kanji")?.Value)
+                            .FirstOrDefault();
+
+            if (wMaxMembersSport != null) {
+                Console.WriteLine($"メンバー人数が最も多い競技名は「{wMaxMembersSport}」です。");
+            } else {
+                Console.WriteLine("競技名が見つかりませんでした。");
+            }
         }
     }
+
 }
