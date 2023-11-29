84f2d0168eb3c056bc0fcce8623a4e57c4772c0b
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-29 13:45:41 +0900

    #462103 [C#テキスト]11章演習問題
    Practice11-1-1のファイルを作成しました。

---
+++ 章末問題11/Practice11-1-1/Program11-1-1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice11_1_1 {
+    /*次のXMLファイルがありまます。このXMLファイルを用いてコードを書いてください。
+    <?xml version="1.0" encoding="utf-8" ?>
+<ballSports>
+　<ballsport>
+　　<name kanji="籠球">バスケットボール</name>
+　　<teammembers>5</teammembers>
+　　<firstplayed>1891</firstplayed>
+　</ballsport>
+　<ballsport>
+　　<name kanji="排球">バレーボール</name>
+　　<teammembers>6</teammembers>
+　　<firstplayed>1895</firstplayed>
+　</ballsport>
+　<ballsport>
+　　<name kanji="野球">ベースボール</name>
+　　<teammembers>9</teammembers>
+　　<firstplayed>1846</firstplayed>
+　</ballsport>
+</ballSports> */
+//Practice11-1-1 XMLファイルを読み込み、競技名とチームメンバーの数の一覧を表示してください。
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
