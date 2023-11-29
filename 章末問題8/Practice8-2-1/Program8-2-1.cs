c534b5cf961b0cf3572b0977eb7998228a61a43a
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-11-29 14:30:08 +0900

    #459603 [C#テキスト]8章演習問題
    Practice8-2-1において、条件分岐の表記を再変更しました。

--- 章末問題8/Practice8-2-1/Program8-2-1.cs
+++ 章末問題8/Practice8-2-1/Program8-2-1.cs
@@ -6,7 +6,7 @@ namespace Practice8_2_1 {
         static void Main(string[] args) {
             Console.WriteLine("次の週の指定曜日を求めます。対応表に従って、0～6の数字を半角で入力してください。\n月曜日:1\n火曜日=2\n水曜日=3\n木曜日=4\n金曜日=5\n土曜日=6\n日曜日=0");
             var wInput = Console.ReadLine();
-            if (!int.TryParse(wInput, out var wIntWeek) || 0 > wIntWeek  || wIntWeek > 6) {
+            if (!int.TryParse(wInput, out var wIntWeek) || wIntWeek < 0 || wIntWeek > 6) {
                 Console.WriteLine("入力が適切ではありません。半角で0～6の数字を入力してください");
                 return;
             }
