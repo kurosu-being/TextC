073d14aa93275dd50a3d7b3cad026bccb3376ddd
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-12-04 11:25:19 +0900

    #459842 [C#テキスト]9章演習問題
    Practice9-1-1において、不要な空行を削除しました。
    Practice9-4-1において、不要な空行を削除しました。

--- 章末問題9/Practice9-1-1/Program9-1-1.cs
+++ 章末問題9/Practice9-1-1/Program9-1-1.cs
@@ -16,8 +16,6 @@ namespace Practice9_1_1 {
                 return;
             }
 
-
-
             int wClassCount = 0;
 
             using (var wReader = new StreamReader(wFilePath)) {
--- 章末問題9/Practice9-4-1/Program9-4-1.cs
+++ 章末問題9/Practice9-4-1/Program9-4-1.cs
@@ -29,7 +29,6 @@ namespace Practice9_4_1 {
                 string wFileName = Path.GetFileNameWithoutExtension(wFile);
                 string wExtension = Path.GetExtension(wFile);
                 string wNewFileName = $"{wFileName}_bak{wExtension}";
-
                 string wNewFilePath = Path.Combine(wTargetDirectory, wNewFileName);
 
                 if (!File.Exists(wNewFilePath)) {
