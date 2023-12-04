9fd8c956fee921b01618d234d535cc537bd1ec94
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-12-04 16:47:23 +0900

    #462103 [C#テキスト]11章演習問題
    Practice11-2-1において、問題文を記載しました。

--- 章末問題11/Practice11-2-1/Program11-2-1.cs
+++ 章末問題11/Practice11-2-1/Program11-2-1.cs
@@ -3,6 +3,35 @@ using System.IO;
 using System.Xml.Linq;
 
 namespace Practice11_2_1 {
+    /*　次のようなXMLファイルがあります。
+    <?xml version="1.0" encoding="utf-8" ?>
+<difficultkanji>
+ <word>
+  <kanji>鬼灯</kanji>
+  <yomi>ほおずき</yomi>
+ </word>
+ <word>
+  <kanji>暖簾</kanji>
+  <yomi>のれん</yomi>
+ </word>
+ <word>
+  <kanji>杜撰</kanji>
+  <yomi>ずさんく/yomi>
+ </word>
+ <word>
+  <kanji>坩堝</kanji>
+  <yomi>るつぼ</yomi>
+ </word>
+</difficultkanji>
+    このXMLファイルを次の形式に変換し、別のファイルに保存してください。
+<?xml version="1.0"　encoding="utf-8" ?>
+<difficultkanji>
+<word kanji="鬼灯"yomi="ほおずき1>
+<word kanji="暖簾"yomi="のれん"1>
+<word kanji="杜撰"yomi="ずさん"1>
+<word kanji="坩堝"yomi="るつぼ!>
+</difficultkanji>
+*/
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("XMLファイルのパスを入力してください。");
