b078095cf30d533ede0511533d6d2d29a438eca3
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-12-06 09:42:34 +0900

    #463298 [C#テキスト]12章演習問題
    Practice12-1-2にEmployeeクラスのコレクションのオブジェクトを追加しました。

---
+++ 章末問題12/Practice12-1-2/EmployeeCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Practice12_1_1;
+
+namespace Practice12_1_2 {
+    /// <summary>
+    /// 従業員のクラス
+    /// </summary>
+    [XmlRoot("employees")]
+    public class EmployeeCollection {
+        [XmlElement(Type = typeof(Employee), ElementName = "employee")]
+        public Employee[] Employees { get; set;}     
+        }
+    }
+
--- 章末問題12/Practice12-1-2/Program12-1-2.cs
+++ 章末問題12/Practice12-1-2/Program12-1-2.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Practice12_1_1;
 
 namespace Practice12_1_2 {
+    //Practice12-1-2 複数のEmployyeeオブジェクトが配列に格納されているとします。この配列をDataContractSerializerクラスを使ってXMLファイルにシリアル化してください。
     class Program {
         static void Main(string[] args) {
+            var wEmployees = new Employee[] {
+                new Employee { ID = 953, Name = "K", HireDate = new DateTime(2023, 4, 1)  },
+                new Employee { ID = 903, Name = "L", HireDate = new DateTime(2023, 4, 2)  },
+                new Employee { ID = 999, Name = "M", HireDate = new DateTime(2023, 4, 3)  }
+            };
         }
     }
 }
