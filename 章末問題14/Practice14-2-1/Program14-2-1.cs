e890671cec926caf8ce2fb6b6ad22c188647f0e5
BEING\kurosu <kurosu@beingcorp.co.jp>
2023-12-22 16:42:14 +0900

    #468890 [C#テキスト]14章演習問題
    Practice14-2-1を実装しました。

---
+++ 章末問題14/Practice14-2-1/Program14-2-1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice14_2_1 {
+    //Practice14-2-1 自分自身のファイルバージョンとアッセンブリバージョンを表示するコンソールアプリケーションを作成してください。
+    class Program {
+        static void Main(string[] args) {
+            var wAssembly = Assembly.GetExecutingAssembly();
+            var wAssemblyVersion = wAssembly.GetName().Version;
+            var wFileVersion = FileVersionInfo.GetVersionInfo(wAssembly.Location);
+            Console.WriteLine($"{wFileVersion.FileMajorPart}{wFileVersion.FileMinorPart}{wFileVersion.FileBuildPart}{wFileVersion.FilePrivatePart}{Environment.NewLine}{wAssemblyVersion.Major}.{wAssemblyVersion.Minor}.{wAssemblyVersion.Build}.{wAssemblyVersion.Revision}");
+        }
+    }
+}
