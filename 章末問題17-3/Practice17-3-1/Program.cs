2edd10bafff6894eaab71178e27942a1c2e3377a
Maki <kurosu@beingcorp.co.jp>
2024-02-02 11:49:37 +0900

    #474409 [C#テキスト]17章演習問題
    Practice17-3-1において、問題文を追記しました。

--- 章末問題17-3/Practice17-3-1/Program.cs
+++ 章末問題17-3/Practice17-3-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿namespace Practice17_3_1 {
+    //Practice17-3-1 17-1-1で作成したプログラムをこの構造（チケットに貼っています)に合うように書き換えてください。
     class Program {
         static void Main(string[] args) {
             var wToHankakuService = new ToHankakuService();
